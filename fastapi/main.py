8ffdb4054e3e2c38cb1c84091e9441bcd64450c5
Daniel Ferreira <dcfer2017@gmail.com>
2025-10-13 11:11:01 -0300

    feat: adiciona fake_db e filtro de posts publicados com parâmetros skip e limit

--- fastapi/main.py
+++ fastapi/main.py
@@ -3,9 +3,43 @@ from datetime import datetime, UTC
 
 app = FastAPI()
 
+fake_db = [
+    {
+        "title": "Criando uma aplicação com FastAPI",
+        "date": datetime.now(UTC),
+        "published": True,
+    },
+    {
+        "title": "Criando uma aplicação com Django",
+        "date": datetime.now(UTC),
+        "publshed": True,
+    },
+    {
+        "title": "Criando uma aplicação com Flask",
+        "date": datetime.now(UTC),
+        "published": True,
+    },
+    {
+        "title": "Criando uma aplicação com Pyramid",
+        "date": datetime.now(UTC),
+        "published": False,
+    },
+]
+
+
+@app.get("/posts")
+def read_posts(
+    published: bool,
+    skip: int = 0,
+    limit: int = len(fake_db),
+):
+    return [
+        post for post in fake_db[skip : skip + limit] if post["published"] is published
+    ]
+
 
 @app.get("/posts/{framework}")
-def read_posts(framework: str):
+def read_framework_posts(framework: str):
     return {
         "posts": [
             {
